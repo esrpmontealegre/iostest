2a7d2edf9c03ed308015c7a4d0eb788990e8412b
esrpmontealegre <esrpmontealegre@gmail.com>
2025-11-06 10:55:57 +0800

    added debug

--- src/app/input-box/input-box.component.ts
+++ src/app/input-box/input-box.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, OnDestroy, ElementRef } from '@angular/core';
 export class InputBoxComponent implements OnInit, OnDestroy {
   private visualViewport: any;
   private resizeHandler: () => void;
+  debugInfo: string = 'Waiting for keyboard...';
 
   constructor(private elementRef: ElementRef) {
     // Initialize the resize handler
@@ -15,12 +16,20 @@ export class InputBoxComponent implements OnInit, OnDestroy {
       if (this.visualViewport) {
         const currentHeight = this.visualViewport.height;
         const containerElement = this.elementRef.nativeElement.querySelector('.input-box-container');
+        const bottomOffset = window.innerHeight - this.visualViewport.height;
         
         if (containerElement) {
           // When keyboard appears, viewport height decreases
           // We want to position our input just above the keyboard
           containerElement.style.position = 'fixed';
-          containerElement.style.bottom = `${window.innerHeight - this.visualViewport.height}px`;
+          containerElement.style.bottom = `${bottomOffset}px`;
+          
+          // Update debug information
+          this.debugInfo = `Viewport Height: ${currentHeight}px\n` +
+                          `Window Height: ${window.innerHeight}px\n` +
+                          `Bottom Offset: ${bottomOffset}px\n` +
+                          `Scale: ${this.visualViewport.scale}\n` +
+                          `Page Top: ${this.visualViewport.pageTop}px`;
         }
       }
     };
