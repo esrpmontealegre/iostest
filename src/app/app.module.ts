ead5612acb7ce5158b2b591793b2e075deef6806
esrpmontealegre <esrpmontealegre@gmail.com>
2025-11-06 10:40:09 +0800

    added inputboxcomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputBoxComponent } from './input-box/input-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputBoxComponent
   ],
   imports: [
     BrowserModule,
